5428ff485991efa0e605375267de4490905b4846
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-02 20:37:20 +0530

    Adding starting class for writing our service.

---
+++ Project_1_HelloWorldSolution/HelloWorldSolution/HelloWorldService/HelloWorldService.cs
@@ -0,0 +1,13 @@
+﻿// In this class we will begin writing our WCF service code.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldService
+{
+    class HelloWorldService
+    {
+    }
+}
