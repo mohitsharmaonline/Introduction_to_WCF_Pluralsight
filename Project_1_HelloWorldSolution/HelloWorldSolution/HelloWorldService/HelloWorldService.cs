556576972e5575290fcd4ac339f41282e0a752ca
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-02 20:42:57 +0530

    Introduction of new class to hold parameter that will be passed as input to service.

--- Project_1_HelloWorldSolution/HelloWorldSolution/HelloWorldService/HelloWorldService.cs
+++ Project_1_HelloWorldSolution/HelloWorldSolution/HelloWorldService/HelloWorldService.cs
@@ -2,11 +2,25 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HelloWorldService
 {
+    // defining a new class, we are going to have a servive called Hello, that will take
+    // a name as input.
+    // To make this class useful in Service contract, we need to annotate it with dataContract attribute.
+    [DataContract]
+    public class Name
+    {
+        // for each field we want to expose, user DataMember attribute.
+        [DataMember]
+        public string First;
+        [DataMember]
+        public string Last;
+    }
+
     class HelloWorldService
     {
     }
