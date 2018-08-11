670519d39ca50254bb40873ee676800de94e8445
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-11 22:06:34 +0530

    incomplete code to add host programetically.

--- Project_2_Writing_a_Simple_Service/ChatSolution/ConsoleHost/Program.cs
+++ Project_2_Writing_a_Simple_Service/ChatSolution/ConsoleHost/Program.cs
@@ -1,10 +1,41 @@
 ﻿
+using EvalServiceLibrary;
+using System;
+using System.ServiceModel;
+
 namespace ConsoleHost
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Service host initialization.
+            ServiceHost host = new ServiceHost(serviceType: typeof(EvalService));
+
+            // adding endpoints to host.
+            host.AddServiceEndpoint(implementedContract: typeof(IEvalService),
+                binding: new BasicHttpBinding(),
+                address: "http://localhost:8080/evals/basic");
+
+            host.AddServiceEndpoint(implementedContract: typeof(IEvalService),
+                binding: new WSHttpBinding(),
+                address: "http://localhost:8080/evals/ws");
+
+            host.AddServiceEndpoint(implementedContract: typeof(IEvalService),
+                binding: new NetTcpBinding(),
+                address: "net.tcp://localhost:8081/evals");
+
+            try
+            {
+                host.Open();
+                Console.ReadLine();
+                host.Close();
+            }
+            catch(Exception ex)
+            {
+
+            }
+
         }
     }
 }
