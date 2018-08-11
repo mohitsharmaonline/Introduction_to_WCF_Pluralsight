31f83c5dcfdc969e6109f4122cb239794e3933c0
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-11 20:35:30 +0530

    completed DataContract class

--- Project_2_Writing_a_Simple_Service/ChatSolution/EvalServiceLibrary/EvalService.cs
+++ Project_2_Writing_a_Simple_Service/ChatSolution/EvalServiceLibrary/EvalService.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EvalServiceLibrary
 {
-    // This class will represent Evaluation.    
+    // This class will represent Evaluation.  
+    // In order to use it within my servicontract, i need to annotate it
+    // with 'DataContract'.
+    [DataContract]
     public class Eval
     {
+        // I need to include each member that i use with 'DataMember'
+        [DataMember]
         public string Submitter;
+        [DataMember]
         public DateTime TimeSent;
+        [DataMember]
         public string Comments;
     }
 
