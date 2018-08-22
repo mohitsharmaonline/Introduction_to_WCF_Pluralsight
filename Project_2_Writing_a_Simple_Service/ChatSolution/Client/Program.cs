78eb02913d63f734975e788cd698819c9f2fb833
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-22 19:57:28 +0530

    Start of Creating, Using and closing channels.

--- Project_2_Writing_a_Simple_Service/ChatSolution/Client/Program.cs
+++ Project_2_Writing_a_Simple_Service/ChatSolution/Client/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Client
 {
     // Start of Demo: Using Service References module.Refer rtf file named 'Demo_Using_Service_Reference'
+    // Start of 'Creating, using and closing channels': So our client now contains an EvalServicereference.
     class Program
     {
         static void Main(string[] args)
