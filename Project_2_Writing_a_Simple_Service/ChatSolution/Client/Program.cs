bf262db093f92c7bf776668afab7249b91abc533
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-22 20:25:09 +0530

    code for channel creation , usage and closing.

--- Project_2_Writing_a_Simple_Service/ChatSolution/Client/Program.cs
+++ Project_2_Writing_a_Simple_Service/ChatSolution/Client/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Client.EvalsServiceReference;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +17,36 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            // Construction of channel.
+            // Take endpoint name from the config file.
+            ChannelFactory<IEvalService> cf =
+                new ChannelFactory<IEvalService>("NetNamedPipeBinding_IEvalService");
+
+            IEvalService channel = cf.CreateChannel();
+            Eval eval = new Eval();
+            eval.Submitter = "Mohit";
+            eval.TimeSent = DateTime.Now;
+            eval.Comments = "I'm liking this...";
+
+            channel.SubmitEval(eval);
+            channel.SubmitEval(eval);
+
+            // Notice that in the signature here you can see that it returns and Eval array.
+            // Now on the service side, we have used List of Evals, but in the actual schema definition
+            // that is part of the WSDL, that's just gonna be represented as a sequence of elements. and so 
+            // on the client side , the client code generator has to decide how to map that back into .net code,
+            // and by default they choose to use arrays insted of Lists. But we can change that later on.
+            Eval[] evals = channel.GetEvals();
+            Console.WriteLine("Number of evals: {0}", evals.Length);
+
+            // When we are done using the channel, we need to close it.
+            // we csn do it by calling close() on the channel
+            // channel.Close(); we won't find this method by default available to us,
+            // because i will only see the operations made available on IEvalService,
+            // In order to get the operation Close, we need to cast it to 'IClientChannel' interface.
+            // Remember that all channels returned by CreateChannel will automatically implement iClientChannel,
+            // as well as service contract type.
+            ((IClientChannel)channel).Close();
         }
     }
 }
