e506ca98539de7f4a374aeb3619c67e8b9e3f9fc
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-08-22 20:01:27 +0530

    Commentary on how to start and agenda for the session.

--- Project_2_Writing_a_Simple_Service/ChatSolution/Client/Program.cs
+++ Project_2_Writing_a_Simple_Service/ChatSolution/Client/Program.cs
@@ -8,6 +8,9 @@ namespace Client
 {
     // Start of Demo: Using Service References module.Refer rtf file named 'Demo_Using_Service_Reference'
     // Start of 'Creating, using and closing channels': So our client now contains an EvalServicereference.
+    // Now we need to start using the API on the client side to constrct a channel, 
+    //and start sending messages to this service.
+    // Start Service if not already, by pressing Ctrl+F5.
     class Program
     {
         static void Main(string[] args)
